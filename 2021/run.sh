b70a402395cfb80672c7ccd3ca62170afe28b5e4
Ernest Gupik <ernestgupik@wp.pl>
2021-12-01 22:01:48 +0100

    2021: Run script: Support passing up to 3 args.
    (those args are passed to the compiled program)

--- 2021/run.sh
+++ 2021/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 dir=$1
+arg1=$2
+arg2=$3
+arg3=$4
 
 if [[ $dir == "" ]] # if the directory is empty, show the usage information
 then
@@ -21,4 +24,4 @@ then
     echo "target directory created"
 fi
 
-rustc "./$dir/main.rs" -o "./$dir/target/main.x" && ./$dir/target/main.x
+rustc "./$dir/main.rs" -o "./$dir/target/main.x" && ./$dir/target/main.x $arg1 $arg2 $arg3
