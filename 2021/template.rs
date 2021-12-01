e66b91392055f19d1778f9489529da2d0f2933ca
Ernest Gupik <ernestgupik@wp.pl>
2021-12-01 23:22:51 +0100

    2021: Update template file

--- 2021/template.rs
+++ 2021/template.rs
@@ -1,17 +1,19 @@
 use std::fs::read_to_string;
 
 fn part1(input_file: String) {
-    dbg!(input_file);
+    let mut answer: usize = 0;
+    println!("Answer: {}", answer);
 }
 
 fn part2(input_file: String) {
-    dbg!(input_file);
+    let mut answer: usize = 0;
+    println!("Answer: {}", answer);
 }
 
 fn main() {
     let part: i32 = 1;
 
-    let input_file: String = read_to_string(format!("input{}.txt", part)).unwrap();
+    let input_file: String = read_to_string("input.txt").unwrap();
 
     if part == 1 { part1(input_file); }
     else if part == 2 { part2(input_file); }
