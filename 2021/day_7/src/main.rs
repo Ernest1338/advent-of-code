fb433b40c3c0fe9c16d687eb8314168a9b2f836c
Ernest Gupik <ernestgupik@wp.pl>
2021-12-07 23:49:05 +0100

    2021: Day 7: Solution

---
+++ 2021/day_7/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs::read_to_string;
+
+fn part1(input_file: String) {
+    let crabs: Vec<usize> = input_file.trim().split(",").collect::<Vec<&str>>().iter().map(|n| n.parse::<usize>().unwrap()).collect();
+    let mut fuel_costs: Vec<usize> = vec![];
+    let positions_to_test: usize = 500;
+    let mut cur_fuel_cost: usize = 0;
+    for position in 1..=positions_to_test {
+        for crab in &crabs {
+            cur_fuel_cost += (crab.to_owned() as i32 - position as i32).abs() as usize;
+        }
+        fuel_costs.push(cur_fuel_cost);
+        cur_fuel_cost = 0;
+    }
+    let answer: usize = fuel_costs.iter().min().unwrap().to_owned();
+    println!("Answer: {}", answer);
+}
+
+fn part2(input_file: String) {
+    let crabs: Vec<usize> = input_file.trim().split(",").collect::<Vec<&str>>().iter().map(|n| n.parse::<usize>().unwrap()).collect();
+    let mut fuel_costs: Vec<usize> = vec![];
+    let positions_to_test: usize = 500;
+    let mut cur_fuel_cost: usize = 0;
+    let mut tmp_fuel: usize;
+    for position in 1..=positions_to_test {
+        for crab in &crabs {
+            tmp_fuel = (crab.to_owned() as i32 - position as i32).abs() as usize;
+            for i in 1..=tmp_fuel {
+                cur_fuel_cost += i;
+            }
+        }
+        fuel_costs.push(cur_fuel_cost);
+        cur_fuel_cost = 0;
+    }
+    let answer: usize = fuel_costs.iter().min().unwrap().to_owned();
+    println!("Answer: {}", answer);
+}
+
+fn main() {
+    let part: i32 = 2;
+
+    let input_file: String = read_to_string("input.txt").unwrap();
+
+    if part == 1 { part1(input_file); }
+    else if part == 2 { part2(input_file); }
+}
