81c736f585110b9f512bc499d1dcf33a623095ea
Ernest Gupik <ernestgupik@wp.pl>
2023-12-07 00:56:59 +0100

    2023: day 5: refactor + part 2

--- 2023/day_5/src/main.rs
+++ 2023/day_5/src/main.rs
@@ -2,55 +2,6 @@
 
 use aoc_std::*;
 
-use std::ops::Range;
-
-#[derive(Debug)]
-struct MapRange {
-    source_range: Range<usize>,
-    destination_range: Range<usize>,
-}
-
-#[derive(Debug)]
-struct Map {
-    map_from: String,
-    map_to: String,
-    ranges: Vec<MapRange>,
-}
-
-impl Map {
-    fn from(map: &str) -> Self {
-        let mut map_lines = map.lines();
-        let map_from_to = map_lines.next().unwrap().split_whitespace().next().unwrap();
-        let map_from = map_from_to.split("-to-").next().unwrap().to_string();
-        let map_to = map_from_to.split("-to-").nth(1).unwrap().to_string();
-        let mut ranges: Vec<MapRange> = Vec::new();
-        for r in map_lines {
-            ranges.push(MapRange::from(r));
-        }
-        Self {
-            map_from,
-            map_to,
-            ranges,
-        }
-    }
-}
-
-impl MapRange {
-    fn from(range: &str) -> Self {
-        let split = range
-            .split_whitespace()
-            .map(|v| v.parse::<usize>().unwrap())
-            .collect::<Vec<usize>>();
-        let destination_range_start = split[0];
-        let source_range_start = split[1];
-        let amount = split[2];
-        Self {
-            source_range: source_range_start..source_range_start + amount,
-            destination_range: destination_range_start..destination_range_start + amount,
-        }
-    }
-}
-
 fn part1(input_file: &str) -> usize {
     let seeds = input_file
         .lines()
@@ -60,35 +11,90 @@ fn part1(input_file: &str) -> usize {
         .skip(1)
         .map(|v| v.parse::<usize>().unwrap())
         .collect::<Vec<usize>>();
-    let mut maps: Vec<Map> = Vec::new();
-    for map in input_file.split("\n\n").skip(1) {
-        maps.push(Map::from(map));
-    }
-    let mut locations: Vec<usize> = Vec::new();
-    for seed in seeds {
-        let mut cur_value = seed;
-        for m in &maps {
-            for r in &m.ranges {
-                if r.source_range.contains(&cur_value) {
-                    let index = r.source_range.clone().position(|x| x == cur_value).unwrap();
-                    cur_value = r.destination_range.clone().nth(index).unwrap();
+    let maps: Vec<Vec<Vec<usize>>> = input_file
+        .split("\n\n")
+        .skip(1)
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|v| {
+            v.lines()
+                .skip(1)
+                .map(|x| {
+                    x.split_whitespace()
+                        .map(|y| y.parse::<usize>().unwrap())
+                        .collect::<Vec<usize>>()
+                })
+                .collect::<Vec<Vec<usize>>>()
+        })
+        .collect();
+    let mut lowest = usize::MAX;
+    for seed in &seeds {
+        let mut cur_seed = seed.to_owned();
+        for map in &maps {
+            for inner_map in map {
+                // TODO: better (if value > min < max something like that)
+                if (inner_map[1]..inner_map[1] + inner_map[2]).contains(&cur_seed) {
+                    cur_seed = cur_seed - inner_map[1] + inner_map[0];
                     break;
                 }
             }
         }
-        locations.push(cur_value);
+        if cur_seed < lowest {
+            lowest = cur_seed;
+        }
     }
-    locations
-        .into_iter()
-        .min_by(|a, b| a.partial_cmp(&b).unwrap_or(std::cmp::Ordering::Less))
-        .unwrap()
+    lowest
 }
 
 fn part2(input_file: &str) -> usize {
-    // well well well. that's not gonna work
-    let mut answer: usize = 0;
-    // for line in input_file.lines() {}
-    answer
+    let seeds = input_file
+        .lines()
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .skip(1)
+        .map(|v| v.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+    let maps: Vec<Vec<Vec<usize>>> = input_file
+        .split("\n\n")
+        .skip(1)
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|v| {
+            v.lines()
+                .skip(1)
+                .map(|x| {
+                    x.split_whitespace()
+                        .map(|y| y.parse::<usize>().unwrap())
+                        .collect::<Vec<usize>>()
+                })
+                .collect::<Vec<Vec<usize>>>()
+        })
+        .collect();
+    let mut seed_ranges: Vec<(usize, usize)> = Vec::new();
+    for s in (0..seeds.len()).step_by(2) {
+        seed_ranges.push((seeds[s], seeds[s + 1]));
+    }
+    let mut lowest = usize::MAX;
+    for range in &seed_ranges {
+        println!("{range:?}");
+        for seed in range.0..range.0 + range.1 {
+            let mut cur_seed = seed.to_owned();
+            for map in &maps {
+                for inner_map in map {
+                    // TODO: better (if value > min < max something like that)
+                    if (inner_map[1]..inner_map[1] + inner_map[2]).contains(&cur_seed) {
+                        cur_seed = cur_seed - inner_map[1] + inner_map[0];
+                        break;
+                    }
+                }
+            }
+            if cur_seed < lowest {
+                lowest = cur_seed;
+            }
+        }
+    }
+    lowest
 }
 
 fn main() {
