5da4cff27efbea461f412050d15c104a2b5d6408
Ernest Gupik <ernestgupik@wp.pl>
2024-12-05 20:36:13 +0100

    2024: day 5 more improvements

--- 2024/day_5/src/main.rs
+++ 2024/day_5/src/main.rs
@@ -8,40 +8,32 @@ enum RuleOutcome {
     None,
 }
 
+fn find_indices(rule: &[usize], update: &[usize]) -> Option<(usize, usize)> {
+    let left_index = update.iter().position(|&x| x == rule[0])?;
+    let right_index = update.iter().position(|&x| x == rule[1])?;
+
+    Some((left_index, right_index))
+}
+
 fn check_rule(rule: &[usize], update: &[usize]) -> RuleOutcome {
-    // println!("{update:?}\t{rule:?}");
-
-    let left_index = match update.iter().position(|&x| x == rule[0]) {
-        Some(v) => v,
-        None => return RuleOutcome::None,
-    };
-
-    let right_index = match update.iter().position(|&x| x == rule[1]) {
-        Some(v) => v,
-        None => return RuleOutcome::None,
-    };
-
-    // println!("L: {left_index:?} R: {right_index:?}");
-
-    if left_index > right_index {
-        return RuleOutcome::Failed;
+    if let Some((left_index, right_index)) = find_indices(rule, update) {
+        if left_index > right_index {
+            RuleOutcome::Failed
+        } else {
+            RuleOutcome::Passed
+        }
+    } else {
+        RuleOutcome::None
     }
-
-    RuleOutcome::Passed
 }
 
 fn all_rules_apply(rules: &[Vec<usize>], update: &[usize]) -> bool {
-    for rule in rules {
-        let res = check_rule(rule, update);
-        if let RuleOutcome::Failed = res {
-            return false;
-        }
-    }
-    true
+    rules
+        .iter()
+        .all(|rule| !matches!(check_rule(rule, update), RuleOutcome::Failed))
 }
 
-fn part1(input_file: &str) -> usize {
-    let mut answer: usize = 0;
+fn parse_input(input_file: &str) -> (Vec<Vec<usize>>, Vec<Vec<usize>>) {
     let input_split = input_file.split("\n\n").collect::<Vec<&str>>();
     let rules: Vec<Vec<usize>> = input_split[0]
         .lines()
@@ -51,14 +43,17 @@ fn part1(input_file: &str) -> usize {
         .lines()
         .map(|l| l.split(",").map(|e| e.parse().unwrap()).collect())
         .collect();
+    (rules, updates)
+}
+
+fn part1(input_file: &str) -> usize {
+    let mut answer: usize = 0;
+    let (rules, updates) = parse_input(input_file);
 
     for update in &updates {
-        // println!("checking {update:?}");
         if !all_rules_apply(&rules, update) {
             continue;
         }
-        // println!("passed");
-
         let middle = update[(update.len() - 1) / 2];
         answer += middle;
     }
@@ -68,46 +63,25 @@ fn part1(input_file: &str) -> usize {
 
 fn part2(input_file: &str) -> usize {
     let mut answer: usize = 0;
-    let input_split = input_file.split("\n\n").collect::<Vec<&str>>();
-    let rules: Vec<Vec<usize>> = input_split[0]
-        .lines()
-        .map(|l| l.split("|").map(|e| e.parse().unwrap()).collect())
-        .collect();
-    let updates: Vec<Vec<usize>> = input_split[1]
-        .lines()
-        .map(|l| l.split(",").map(|e| e.parse().unwrap()).collect())
-        .collect();
+    let (rules, mut updates) = parse_input(input_file);
 
-    for update in &updates {
-        let mut update = update.clone();
+    for update in &mut updates {
         let mut fixed = false;
-        // println!("handling update: {update:?}");
+
         while !all_rules_apply(&rules, &update) {
             for rule in &rules {
                 if let RuleOutcome::Failed = check_rule(rule, &update) {
-                    // println!("failed rule: {rule:?}");
-
                     // switch places
-
-                    let left_index = match update.iter().position(|&x| x == rule[0]) {
-                        Some(v) => v,
-                        None => continue,
-                    };
-
-                    let right_index = match update.iter().position(|&x| x == rule[1]) {
-                        Some(v) => v,
-                        None => continue,
-                    };
-
-                    update.swap(left_index, right_index);
-                    fixed = true;
+                    if let Some((left_index, right_index)) = find_indices(rule, &update) {
+                        update.swap(left_index, right_index);
+                        fixed = true;
+                    }
                 }
             }
         }
-        // println!("fixed update: {update:?}");
-        let middle = update[(update.len() - 1) / 2];
-        // println!("middle: {middle:?}");
+
         if fixed {
+            let middle = update[(update.len() - 1) / 2];
             answer += middle;
         }
     }
