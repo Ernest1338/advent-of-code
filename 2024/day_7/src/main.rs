28dfc458db8b0d5428af111d7d9c61962126bb36
Ernest Gupik <ernestgupik@wp.pl>
2024-12-08 01:12:57 +0100

    2024: day 7 fixes and improvements

--- 2024/day_7/src/main.rs
+++ 2024/day_7/src/main.rs
@@ -1,7 +1,6 @@
 #![allow(dead_code, unused_variables, unused_mut)]
 
 use aoc_std::*;
-use itertools::Itertools;
 use rayon::prelude::*;
 
 #[derive(Debug)]
@@ -24,63 +23,92 @@ fn parse(input: &str) -> Vec<Equation> {
     out
 }
 
+fn append_number(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a * 10;
+    }
+
+    let mut digits_in_b = 0;
+    let mut temp_b = b;
+
+    while temp_b > 0 {
+        temp_b /= 10;
+        digits_in_b += 1;
+    }
+
+    a * 10_usize.pow(digits_in_b as u32) + b
+}
+
 fn check_possibilities(equation: &Equation, symbols: &[char]) -> bool {
-    println!("[checking] {equation:?}");
+    // println!("[checking] {equation:?}");
     let combinations_len = equation.numbers.len() - 1;
-    let combinations = symbols
-        .iter()
-        .copied()
-        .combinations_with_replacement(combinations_len)
-        .flat_map(|combo| combo.into_iter().permutations(combinations_len))
-        .unique();
-    for combination in combinations {
+
+    let mut combination = vec![symbols[0]; combinations_len];
+    let mut indices = vec![0; combinations_len];
+
+    loop {
+        // println!("combination: {combination:?}");
         let mut sum = equation.numbers[0];
+        let mut valid = true;
+
         for (i, nums) in equation.numbers.windows(2).enumerate() {
             let symbol = combination[i];
             match symbol {
-                '+' => {
-                    sum += nums[1];
-                }
-                '*' => {
-                    sum *= nums[1];
-                }
-                '|' => {
-                    sum = sum * 10 + nums[1];
-                }
+                '+' => sum += nums[1],
+                '*' => sum *= nums[1],
+                '|' => sum = append_number(sum, nums[1]),
                 _ => (),
             }
             if sum > equation.target {
+                valid = false;
                 break;
             }
         }
-        // println!("sum: {sum}\tcombination: {combination:?}");
-        if sum == equation.target {
-            println!("[result] true\t{equation:?}");
+
+        if valid && sum == equation.target {
+            // println!("[result] true\t{equation:?}\t{combination:?}\n");
             return true;
         }
+
+        let mut carry = true;
+        for i in (0..combinations_len).rev() {
+            if carry {
+                indices[i] += 1;
+                if indices[i] < symbols.len() {
+                    combination[i] = symbols[indices[i]];
+                    carry = false;
+                } else {
+                    indices[i] = 0;
+                    combination[i] = symbols[0];
+                }
+            }
+        }
+
+        if carry {
+            break;
+        }
     }
-    println!("[result] false\t{equation:?}");
+
+    // println!("[result] false\t{equation:?}\n");
     false
 }
 
 fn part1(input_file: &str) -> usize {
     let equations = parse(input_file);
-    let mut answer = 0;
 
     equations
         .par_iter()
-        .filter(|equation| check_possibilities(&equation, &['+', '*']))
+        .filter(|equation| check_possibilities(&equation, &['*', '+']))
         .map(|equation| equation.target)
         .sum()
 }
 
 fn part2(input_file: &str) -> usize {
     let equations = parse(input_file);
-    let mut answer = 0;
 
     equations
         .par_iter()
-        .filter(|equation| check_possibilities(&equation, &['+', '*', '|']))
+        .filter(|equation| check_possibilities(&equation, &['*', '+', '|']))
         .map(|equation| equation.target)
         .sum()
 }
@@ -88,6 +116,6 @@ fn part2(input_file: &str) -> usize {
 fn main() {
     let input_file = load_file("input.txt");
 
-    // println!("[Part 1] Answer: {}", part1(&input_file));
+    println!("[Part 1] Answer: {}", part1(&input_file));
     println!("[Part 2] Answer: {}", part2(&input_file));
 }
